fc0a53eff897d2bfdba4e244bc48f30813a60342
Claromes <claromes@hey.com>
2023-09-15 20:29:48 -0300

    fix alt return and add url option

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ st.set_page_config(
 
         - mobile friendly
         - multiple images
+        - image via upload or URL
         - translation for multiple languages
         - copy to clipboard
 
@@ -54,82 +55,114 @@ metadata = (('authorization', 'Key ' + PAT),)
 userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
 
 def translate(text):
-        translator = Translator()
-        translate = translator.translate((text), src='en', dest=code)
-        return translate.text
+    translator = Translator()
+    translate = translator.translate((text), src='en', dest=code)
+    return translate.text
+
+def show_result(image, alt):
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.image(image)
+
+    with col2:
+        st.caption('english alt')
+        st.code(alt.capitalize(), language='text')
+
+        if not code =='en':
+            st.caption(f'{alt_lang.lower()} alt')
+            st.code(translate(alt), language='text')
 
 st.title('too lazy to write alt', anchor=False)
 st.write('[![general-english-image-caption-blip-2-6_7B](https://clarifai.com/api/salesforce/blip/models/general-english-image-caption-blip-2-6_7B/badge)](https://clarifai.com/salesforce/blip/models/general-english-image-caption-blip-2-6_7B)')
+st.columns(1)
 
-st.write('upload images')
-images = st.file_uploader('upload images', accept_multiple_files=True, label_visibility='collapsed')
-st.caption('*we do not store your images')
+uploaded_files = st.file_uploader('upload images', accept_multiple_files=True, help='we do not store your images')
 
-st.write('alt language other than english')
-col1, col2 = st.columns(2)
+url = st.text_input('paste an image URL')
 
-with col1:
-    alt_lang = st.selectbox(
-        'alt language other than english',
-        (lang[0] for lang in langs),
-        label_visibility='collapsed'
-    )
+alt_lang = st.selectbox(
+    'alt language',
+    (lang[0] for lang in langs)
+)
 
 for lang in langs:
     if alt_lang == lang[0]:
         code = lang[1]
 
+_, col2, _ = st.columns([2, 8, 2])
+
 with col2:
+    st.columns(1)
     button = st.button("generate alt because I'm too lazy", type='primary', use_container_width=True)
 
 st.divider()
 
-if images and button:
-    temp_dir = tempfile.TemporaryDirectory()
+if button:
+    try:
+        with st.spinner('generating...'):
+            if uploaded_files:
+                temp_dir = tempfile.TemporaryDirectory()
 
-    for i, image in enumerate(images):
-        bytes_data = image.read()
+                for i, image in enumerate(uploaded_files):
+                    bytes_data = image.read()
 
-        image_io = Image.open(io.BytesIO(bytes_data))
+                    image_io = Image.open(io.BytesIO(bytes_data))
 
-        temp_image_path = os.path.join(temp_dir.name, image.name)
-        image_io.save(temp_image_path)
+                    temp_image_path = os.path.join(temp_dir.name, image.name)
+                    image_io.save(temp_image_path)
 
-        # IMAGE_URL = 'https://www.w3schools.com/html/pic_trulli.jpg'
-        IMAGE_URL = temp_image_path
+                    IMAGE_FILE_LOCATION = temp_image_path
 
-        response = stub.PostModelOutputs(
-            service_pb2.PostModelOutputsRequest(
-                user_app_id=userDataObject,
-                model_id=MODEL_ID,
-                version_id=MODEL_VERSION_ID,
-                inputs=[
-                    resources_pb2.Input(
-                        data=resources_pb2.Data(
-                            image=resources_pb2.Image(
-                                url=IMAGE_URL
-                            )
-                        )
+                    with open(IMAGE_FILE_LOCATION, "rb") as f:
+                        file_bytes = f.read()
+
+                    response = stub.PostModelOutputs(
+                        service_pb2.PostModelOutputsRequest(
+                            user_app_id=userDataObject,
+                            model_id=MODEL_ID,
+                            version_id=MODEL_VERSION_ID,
+                            inputs=[
+                                resources_pb2.Input(
+                                    data=resources_pb2.Data(
+                                        image=resources_pb2.Image(
+                                            base64=file_bytes
+                                        )
+                                    )
+                                )
+                            ]
+                        ),
+                        metadata=metadata
                     )
-                ]
-            ),
-            metadata=metadata
-        )
 
-        if response.status.code != status_code_pb2.SUCCESS:
-            st.error(f'Post model outputs failed, status: ' + response.status.description)
+                    if response.status.code != status_code_pb2.SUCCESS:
+                        st.error(f'Post model outputs failed, status: ' + response.status.description)
 
-        alt = response.outputs[0].data.text.raw
+                    alt = response.outputs[0].data.text.raw
 
-        col1, col2 = st.columns(2)
+                    show_result(temp_image_path, alt)
 
-        with col1:
-            st.image(image)
+            if url:
+                response = stub.PostModelOutputs(
+                    service_pb2.PostModelOutputsRequest(
+                        user_app_id=userDataObject,
+                        model_id=MODEL_ID,
+                        version_id=MODEL_VERSION_ID,
+                        inputs=[
+                            resources_pb2.Input(
+                                data=resources_pb2.Data(
+                                    image=resources_pb2.Image(
+                                        url=url
+                                    )
+                                )
+                            )
+                        ]
+                    ),
+                    metadata=metadata
+                )
 
-        with col2:
-            st.caption('english alt')
-            st.code(alt.capitalize(), language='text')
+                alt = response.outputs[0].data.text.raw
 
-            if not code =='en':
-                st.caption(f'{alt_lang.lower()} alt')
-                st.code(translate(alt), language='text')
+                show_result(url, alt)
+    except Exception as e:
+        st.error(f'An error has occurred: {e}')
--- readme.md
+++ readme.md
@@ -1,9 +1,17 @@
 # too lazy to write alt
 
+clarifai docs: https://docs.clarifai.com/api-guide/predict/images#predict-via-bytes
+paper: https://arxiv.org/pdf/2201.12086.pdf
+
+image specs:
+- up to 20MB
+
 roadmap:
 
 - [ ] multiples dest langs
-- [ ] url and upload options
+- [ ] multiples urls (comma-separated)
+- [x] url and upload options
 - [x] copy to clipboard
 - [x] style
 - [ ] specs
+- [ ] concepts output
